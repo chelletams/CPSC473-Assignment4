f80fe29007e0077441b16b4c4aad8016e34eff61
chelletams <ymtamaki17@gmail.com>
2016-10-31 18:07:50 -0700

    add files

---
+++ server.js
@@ -0,0 +1,117 @@
+var express = require("express"),
+	http = require("http"),
+	redis = require("redis"),
+	mongoose = require("mongoose"),
+	counts = {},
+	app = express(),
+	bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + "/client"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var redisClient = redis.createClient();
+redisClient.set("right", 0);
+redisClient.set("wrong", 0);
+
+// connect to the data store in mongo
+mongoose.connect("mongodb://localhost/trivia");
+
+var triviaSchema = mongoose.Schema( {
+	"question" : String,
+	"answerId" : Number,
+	"answer" : String
+});
+
+var Trivia = mongoose.model("Trivia", triviaSchema);
+
+http.createServer(app).listen(3000);
+console.log("Server listening on port 3000");
+
+app.get("/questions", function(req, res) {
+	Trivia.find({}, function(err, trivias) {
+		console.log(trivias.question);
+		res.json(trivias);
+	});
+});
+
+app.post("/questions", function (req, res) {
+	console.log(req.body);
+	var newQuestions = new Trivia({"question":req.body.question,
+								   "answerId":req.body.answerId,
+								   "answer":req.body.answer});
+
+	newQuestions.save(function(err, result) {
+		if(err !== null) {
+			console.log(err);
+			res.send("ERROR");
+		}
+		else {
+			Trivia.find({}, function(err, result) {
+				if(err !== null) {
+					res.send("ERROR");
+				}   
+				res.json({result: "Question accepted."});
+			});
+		}
+	});
+});
+
+app.post("/answer", function (req, res) {
+	var jsonObject,
+		answerId,
+		answer;
+
+	req.on("data", function(data) {
+		jsonObject = JSON.parse(data);
+		answerId = parseInt(jsonObject.answerId, 10);
+		answer = jsonObject.answer;
+
+	Trivia.findOne({"answerId":answerId}, function (err, result) {
+		if(err !== null) {
+			console.log("ERROR");
+		}
+		else {
+			if(answer === result.answer) {
+				var correct = true;
+				redisClient.incr("right", function(err, value) {
+					if(err) {
+						console.log("ERROR");
+					}
+					else {
+						console.log("right: " + value);
+					}
+				});
+			}
+			else {
+				correct = false;
+				redisClient.incr("wrong", function(err, value) {
+					if(err) {
+						console.log("ERROR");
+					}
+					else {
+						console.log("wrong: " + value);
+					}
+				});
+			}
+			return res.json({correct: correct});
+		}
+		});
+	});
+});
+
+app.get("/score", function(req, res) {
+	var right,
+		wrong;
+
+	redisClient.mget(["right", "wrong"], function(err, result) {
+		if(err !== null) {
+			console.log("ERROR");
+		}
+		else {
+			right = parseInt(result[0], 10) || 0;
+			wrong = parseInt(result[1], 10) || 0;
+		}
+		return res.json({right: right, wrong: wrong});
+	});
+});
