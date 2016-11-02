6d97cda50e7b2268431254cdd5747868352b610e
chelletams <ymtamaki17@gmail.com>
2016-11-01 22:49:14 -0700

    Updated client-side apps

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,9 +1,7 @@
 var main = function (triviaObjects) {
 	"use strict";
 
-	var trivias = triviaObjects.map(function (trivia) {
-		return trivia.question;
-	});
+	var answerId = 1;
 
 	$("#answerQuestion").on("click", function(event) {
 		window.location.href = "trivia.html";
@@ -13,6 +11,82 @@ var main = function (triviaObjects) {
 		window.location.href = "question.html";
 	});
 
+	var getQuestion = function() {
+		$.get("/questions", function(data) {
+			console.log(data);
+			if(data) {
+				$("#question").html(data.question);
+				answerId = data.answerId;
+			}
+			else {
+				console.log("There is no question in the database.");
+			}
+		});
+	};
+
+	$("#getQuestionButton").on("click", function() {
+		getQuestion();
+	});
+
+	var getAnswer = function() {
+		var jsonData,
+			result;
+
+		if($("#answer").val() !== " ") {
+			jsonData = JSON.stringify({answer:answer, answerId:answerId});
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "/answer",
+			dataType: "json",
+			data: jsonData,
+			success: function(response) {
+				console.log(response);
+				result = response.correct ? "True":"False";
+				$("#getAnswer").html(result);
+			},
+			contentType: "application/json"
+		});
+		$("#answer").val("");
+	};
+
+	$("#getAnswerButton").on("click", function() {
+		getAnswer();
+	});
+
+	$("#getAnswerButton").on("keypress", function(event) {
+		if(event.keyCode === 13) {
+			getAnswer();
+		}
+	});
+
+	var makeUpQuestion = function() {
+		var qstn = $("#makeQuestion").val(),
+			ans = $("#createAnswer").val(),
+			jsonData = JSON.stringify({qstn:qstn, ans:ans});
+
+		$.ajax({
+			type: "POST",
+			url: "/questions",
+			dataType: "json",
+			data: jsonData,
+			success: function(response) {
+				console.log(response);
+			},
+			contentType: "application/json"
+		});
+	};
+
+	$("#submitQuestion").on("click", function() {
+		makeUpQuestion();
+	});
+
+	$("#submitQuestions").on("keypress", function(event) {
+		if(event.keyCode === 13) {
+			makeUpQuestions();
+		}
+	});
 };
 
 $(document).ready(main);
